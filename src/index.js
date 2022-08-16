9da5881555ae0461764bfb8205b94e8a127e0502
Anmol <lomeshgajbhiye@gmail.com>
2022-08-16 23:08:06 +0530

    added succesfully

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://gajbhiyeanmol:OQoGsw19lktYkPKI@cluster0.xjvkna0.mongodb.net/Anmol123-DB?retryWrites=true&w=majority",
+{
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
